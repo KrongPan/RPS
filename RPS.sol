ca5b0f555484f75c1b563a786bab438c36949142
Panyawat_KRONGKITICHU <pan.kkt@gmail.com>
2024-02-12 20:46:05 +0700

    add withdraw() function
    เพื่แเเก้ปัญหากรณี ลงเงินเข้าร่วมไปเเล้วเเต่ไม่มีผู้เล่นอีกคนเข้ามา

--- RPS.sol
+++ RPS.sol
@@ -6,11 +6,23 @@ contract RPS {
     struct Player {
         uint choice; // 0 - Rock, 1 - Paper , 2 - Scissors, 3 - undefined
         address addr;
+        uint depositTime;
     }
     uint public numPlayer = 0;
     uint public reward = 0;
     mapping (uint => Player) public player;
     uint public numInput = 0;
+    uint public withdrawTime = 10 seconds;
+
+    function checkIfRegis() private view returns (uint) {
+        if (msg.sender == player[0].addr) {
+            return 0;
+        } else if (msg.sender == player[1].addr) {
+            return 1;
+        } else {
+            revert("Sender is not a registered player");
+        }
+    }
 
     function addPlayer() public payable {
         require(numPlayer < 2);
@@ -18,19 +30,25 @@ contract RPS {
         reward += msg.value;
         player[numPlayer].addr = msg.sender;
         player[numPlayer].choice = 3;
+        player[numPlayer].depositTime = block.timestamp;
         numPlayer++;
+    }
+
+    function withdraw() public payable {
+        require(numPlayer == 1);
+        uint idx;
+        idx = checkIfRegis();
+        require(block.timestamp >= player[idx].depositTime + withdrawTime);
+        address payable account0 = payable(player[0].addr);
+        account0.transfer(reward);
+        reward -= 1 ether;
+        numPlayer--;
     }
 
     function input(uint choice) public  {
         require(numPlayer == 2);
         uint idx;
-        if (msg.sender == player[0].addr) {
-            idx = 0;
-        } else if (msg.sender == player[1].addr) {
-            idx = 1;
-        } else {
-            revert("Sender is not a registered player");
-        }
+        idx = checkIfRegis();
         require(choice == 0 || choice == 1 || choice == 2);
         player[idx].choice = choice;
         numInput++;
